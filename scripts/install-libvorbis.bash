b897222e29c60bb2c48ba2fae3b2c406522886ed
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-26 21:15:37 -0400

    Added install script for libvorbis

---
+++ scripts/install-libvorbis.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+make uninstall
+make distclean
+make clean
+./autogen.sh
+./configure                                 \
+    --prefix="${FF_PREFIX}"                 \
+    --build="${FF_BUILD}"                   \
+    --host="${FF_HOST}"                     \
+    --enable-dependency-tracking            \
+    --enable-shared="yes"                   \
+    --enable-static="no"                    \
+    --enable-fast-install="no"              \
+    --disable-oggtest                       \
+    --with-pic="pic"                        \
+    --with-sysroot="${FF_SYS_ROOT}"         \
+    CFLAGS="-m64"
+
+make -j12 install
