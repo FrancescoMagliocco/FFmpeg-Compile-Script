45acd73e8f91f793f96a656b4599525025b3ffc6
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-30 18:22:06 -0400

    Fixed RepoBase

--- RepoBase.py
+++ RepoBase.py
@@ -11,22 +11,7 @@ class RepoTool(Enum):
     UND_TOOL = 'Undetermined'
 
 class RepoBase(ABC):
-    def __init__(self):
-        self._repo_tool = RepoTool.UND_TOOL
-        self._repo_url = 'Not specified'
-
-    def get_repo_tool(self):
-        return self._repo_tool
-
-    def get_repo_url(self):
-        return self._repo_url
-
-    repo_tool = property(get_repo_tool)
-    repo_url = property(get_repo_url)
-
-class Libx264(RepoBase):
-    def __init__(self):
-        self._repo_tool = RepoTool.GIT_TOOL
-        self._repo_url = 'https://git.videolan.org/git/x264.git'
-
-print(Libx264.repo_url)
+    def __init__(self, repo_tool=RepoTool.UND_TOOL, repo_url='Not specified'):
+        super().__init__()
+        self.repo_tool = repo_tool
+        self.repo_url = repo_url
