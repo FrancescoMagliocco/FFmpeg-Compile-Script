498a366e906e5a310e132749563adac3388d5a14
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-30 14:54:20 -0400

    Added libmp3lame.py script

---
+++ scripts/libmp3lame.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.6
+
+import os
+
+CONFIG: str = ("mkdir -p build/libmp3lame/; cd build/libmp3lame; ../../repos/libmp3lame/configure "
+        + "--prefix='{0}' ".format(os.path.expandvars('${FF_PREFIX}'))
+        + "--build='{0}' ".format(os.path.expandvars('${FF_BUILD}'))
+        + "--host='{0}' ".format(os.path.expandvars('${FF_HOST}'))
+        + "--enable-static='no' "
+        + "--enable-nasm "
+        + "--disable-rpath "
+        + "--disable-cpml "
+        + "--disable-gtktest "
+        + "--disable-frontend "
+        + "--with-pic='pic' "
+        + "--with-sysroot='{0}' ".format(os.path.expandvars('${FF_SYS_ROOT}'))
+        + "CFLAGS='-m64'")
