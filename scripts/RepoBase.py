12311db863a965c6e0b4302b1d8174e871254a6b
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-30 18:53:32 -0400

    Added get_config()

--- scripts/RepoBase.py
+++ scripts/RepoBase.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-
-from abc import ABC
+from abc import ABC, abstractmethod
 from enum import Enum
 
 class RepoTool(Enum):
@@ -11,7 +9,11 @@ class RepoTool(Enum):
     UND_TOOL = 'Undetermined'
 
 class RepoBase(ABC):
-    def __init__(self, repo_tool=RepoTool.UND_TOOL, repo_url='Not specified'):
-        super().__init__()
-        self.repo_tool = repo_tool
-        self.repo_url = repo_url
+#    def __init__(self, repo_tool=RepoTool.UND_TOOL, repo_url='Not specified'):
+#        super().__init__()
+#        self.repo_tool = repo_tool
+#        self.repo_url = repo_url
+
+    @abstractmethod
+    def get_config(self):
+        raise NotImplementedError()
