42b76f4e4b5ac3fc4c7c1c917742556312e38f00
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-30 18:18:26 -0400

    Don't ask

--- scripts/libx264.py
+++ scripts/libx264.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3.6
 
+
+
+
+
 import os
+
+X264_REPO_URL: str = "https://git.videolan.org/git/x264.git"
+GPAC_REPO_URL: str = "https://Github.com/gpac/gpac.git"
+
 
 def download_gpac():
     command: str = "git clone https://Github.com/gpac/gpac.git ../repos/gpac"
