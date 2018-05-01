e6c6fd18a551adf4f62dcc8ff24a6f5584b65993
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-05-01 01:37:53 -0400

    Color is wokring now.  Implementing the formats for each level now

--- scripts/VLogger.py
+++ scripts/VLogger.py
@@ -27,6 +27,25 @@ class VColors(Enum):
     def has_name(cls, name):
         return any(name == item.name for item in cls)
 
+    @classmethod
+    def _get_value(cls, name):
+        if cls.has_name(name):
+            return cls[name].value
+        else:
+            sys.exit(1)
+
+    _ALL_VCOLOR_FMTS = {
+            'CRITICAL': '{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
+                _get_value(cls, 'CRITICAL'), NORMAL.value, WARNING.value, GREEN.value)
+            }
+
+    @classmethod
+    def get_fmt(cls, name):
+        if cls.has_name(name):
+            return cls._ALL_VCOLOR_FMTS[name]
+        else:
+            sys.exit(1)
+
 CRITICAL_FMT = (
         '{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
             VColors.CRITICAL, VColors.NORMAL, VColors.WARNING, VColors.GREEN))
@@ -38,31 +57,34 @@ INFO_FMT = '{0}%(msg)s{1}'.format(VColors.INFO, VColors.NORMAL)
 DEBUG_FMT = '{0}DEBUG{1}:%(msg)s'.format(VColors.DEBUG, VColors.NORMAL)
 
 # https://stackoverflow.com/a/384125
-class VerboseFormatter(logging.Formatter):
+class VFormatter(logging.Formatter):
     def __init__(self, msg, use_color=True):
+        print("Entered '__init__()' in class 'VerboseFormatter'..")
         logging.Formatter.__init__(self, msg)
         self.use_color = use_color
 
     def format(self, record):
+        print("Entered 'format()' in class 'VerboseFormatter'..")
         levelname = record.levelname
+
         if self.use_color and VColors.has_name(levelname):
-            print("BALLS")
-            record.level = VColors[levelname].value
+#            record.levelname = VColors[levelname].value
+            record.levelname = VColors.get_fmt(levelname)
         return logging.Formatter.format(self, record)
 
 # https://stackoverflow.com/a/384125
-class VerboseLogger(logging.Logger):
+class VLogger(logging.Logger):
     FORMAT = '%(levelname)s\t%(message)s'
     def __init__(self, name):
-        print("LKSJDF")
+        print("Entered '__init__()' in class 'VerboseLogger'..")
         logging.Logger.__init__(self, name, logging.DEBUG)
 
-        color_formatter = VerboseFormatter(self.FORMAT)
+        color_formatter = VFormatter(self.FORMAT)
         console = logging.StreamHandler()
-        print("HERE")
         console.setFormatter(color_formatter)
 
         self.addHandler(console)
 
-logging.setLoggerClass(VerboseLogger)
-logging.debug("Test")
+logging.setLoggerClass(VLogger)
+test = logging.getLogger('VLogger')
+test.critical("Test")
