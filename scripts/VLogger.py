efe641572caa7e3491b534125f1bd8147708ba7b
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-04-30 22:59:53 -0400

    Changed approach for coloring..

--- scripts/VLogger.py
+++ scripts/VLogger.py
@@ -4,6 +4,8 @@
 """Verbose Logger"""
 
 import logging
+import sys
+from enum import Enum
 
 __author__ = "Francesco Magliocco (aka Cmptr)"
 __license__ = "GPLv3"
@@ -11,51 +13,54 @@ __version__ = "0.0.1"
 __maintainer__ = "Francesco Magliocco (aka Cmptr)"
 __status__ = "Development"
 
-class VColors:
-    _ESC = '\e[38;5;{0}m'
-    DARK_RED = _ESC.format(88)
-    CRITICAL = DARK_RED
-    RED = _ESC.format(196)
-    ERROR = RED
-    ORANGE = _ESC.format(202)
-    WARNING = ORANGE
-    YELLOW = _ESC.format(226)
-    INFO = YELLOW
-    MAGENTA = _ESC.format(201)
-    DEBUG = MAGENTA
+class VColors(Enum):
+    _ESC = '\033[38;5;{0}m'
+    CRITICAL = _ESC.format(88)
+    ERROR = _ESC.format(196)
+    WARNING = _ESC.format(202)
+    INFO = _ESC.format(226)
+    DEBUG = _ESC.format(201)
     GREEN = _ESC.format(46)
-    NORMAL = '\e[39m'
+    NORMAL = '\033[39m'
 
-# https://stackoverflow.com/a/14859558
-class VFormatter(logging.Formatter):
-    CRITICAL_FMT = (
-            '{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
-                VColors.CRITICAL, VColors.NORMAL, VColors.ORANGE, VColors.GREEN))
-    ERROR_FMT = (
-            '{0}ERROR{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
-                VColors.ERROR, VColors.NORMAL, VColors.ORANGE, VColors.GREEN))
-    WARNING_FMT = '{0}WARNING{1}:%(msg)s'.format(VColors.WARNING, VColors.NORMAL)
-    INFO_FMT = '{0}%(msg)s{1}'.format(VColors.INFO, VColors.NORMAL)
-    DEBUG_FMT = '{0}DEBUG{1}:%(msg)s'.format(VColors.DEBUG, VColors.NORMAL)
+    @classmethod
+    def has_name(cls, name):
+        return any(name == item.name for item in cls)
 
-    def __init__(self):
-        super().__init__(fmt='%(levelno)d: %(msg)s', datefmt=None, style='%')
+CRITICAL_FMT = (
+        '{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
+            VColors.CRITICAL, VColors.NORMAL, VColors.WARNING, VColors.GREEN))
+ERROR_FMT = (
+        '{0}ERROR{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
+            VColors.ERROR, VColors.NORMAL, VColors.WARNING, VColors.GREEN))
+WARNING_FMT = '{0}WARNING{1}:%(msg)s'.format(VColors.WARNING, VColors.NORMAL)
+INFO_FMT = '{0}%(msg)s{1}'.format(VColors.INFO, VColors.NORMAL)
+DEBUG_FMT = '{0}DEBUG{1}:%(msg)s'.format(VColors.DEBUG, VColors.NORMAL)
+
+# https://stackoverflow.com/a/384125
+class VerboseFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
 
     def format(self, record):
-        fmt_orig = self._style._fmt
+        levelname = record.levelname
+        if self.use_color and VColors.has_name(levelname):
+            record.level = VColors[levelname].value
+        return logging.Formatter.format(self, record)
 
-        if record.levelno == logging.DEBUG:
-            self._style._fmt = VFormatter.DEBUG_FMT
-        elif record.levelno == logging.INFO:
-            self._style._fmt = VFormatter.INFO_FMT
-        elif record.levelno == logging.WARNING:
-            self._style._fmt = VFormatter.WARNING_FMT
-        elif record.levelno == logging.ERROR:
-            self._style._fmt = VFormatter.ERROR_FMT
-        elif record.levelno == logging.CRITICAL:
-            self._style._fmt = VFormatter.CRITICAL_FMT
+# https://stackoverflow.com/a/384125
+class VerboseLogger(logging.Logger):
+    FORMAT = "[%(name)-20s][%(levelname)-18s]  %(message)s (%(filename)s:%(lineno)d)"
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
 
-        result = logging.Formatter.format(self, record)
-        self._style._fmt = fmt_orig
+        color_formatter = VerboseFormatter(self.FORMAT)
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
 
-        return result
+        self.addHandler(console)
+
+logging.basicConfig(level=logging.DEBUG)
+logging.setLoggerClass(VerboseLogger)
+logging.debug("Test")
