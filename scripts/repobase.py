832a893ad345d17aa8eddeb52b132d3ccf7b2471
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2018-05-03 02:56:48 -0400

    Added 'get_update_repo()' and dictionary mapping the update command to the repository type.

--- scripts/repobase.py
+++ scripts/repobase.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
+import logging
 from enum import Enum
+from abc import ABC, abstractmethod
+from typing import List
 
 class RepoTool(Enum):
     CURL_TOOL = 'curl'
@@ -9,12 +11,25 @@ class RepoTool(Enum):
     UND_TOOL = 'Undetermined'
 
 class RepoBase(ABC):
+
+    # TODO: Raise some type of 'warning' or 'notice' if key is either
+    # 'RepoTool.CURL_TOOL' or 'RepoTool.UDN_TOOL'.
+    _REPOTOOL_TO_UPDATE_CMD = {
+            RepoTool.CURL_TOOL: [''],
+            RepoTool.GIT_TOOL: ['git pull'],
+            RepoTool.SVN_TOOL: ['svn up'],
+            RepoTool.HG_TOOL: ['hg pull', 'hg update'],
+            RepoTool.UND_TOOL: ['']
+            }
+
     def __init__(
             self,
+            name,
             repo_tool=RepoTool.UND_TOOL,
             repo_url='Not specified',
             switch='Not specified'):
         super().__init__()
+        self.name = name
         self.repo_tool = repo_tool
         self.repo_url = repo_url
         self.switch = switch
@@ -27,6 +42,8 @@ class RepoBase(ABC):
     def get_repo_download(self):
         raise NotImplementedError("'get_repo_download()' Not Implemented!")
 
-    @abstractmethod
-    def update_repo(self):
-        raise NotImplementedError("'update_repo()' Not Implemented!")
+    # Gets the update command for the corresponding repository type.
+    def get_update_repo(self) -> List[str]:
+        logging.debug("Entered 'update_repo!")
+        logging.info("Updating repository '%d' ...", self.name)
+        return self._REPOTOOL_TO_UPDATE_CMD[self.repo_tool]
