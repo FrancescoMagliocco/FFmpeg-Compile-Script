e22b1d9507c78fe9b7f5c0b787fd798cd0050620
FrancescoMagliocco <Magliocco.W.Francesco@gmail.com>
2018-05-05 16:46:51 -0400

    pep

--- scripts/vlogger.py
+++ scripts/vlogger.py
@@ -14,6 +14,7 @@ __maintainer__ = "Francesco Magliocco (aka Cmptr)"
 __status__ = "Development"
 
 class VColors(Enum):
+    """Colors for Verbose Logger"""
     _CLR_SEQ = '\033[38;5;'
     _FMT = '{0:s}{1:d}m[{0:s}{2:d}m{3:s}{0:s}{1:d}m]'
     CRITICAL = _FMT.format(_CLR_SEQ, 255, 88, 'CRITICAL')
@@ -26,20 +27,30 @@ class VColors(Enum):
 
     @classmethod
     def has_name(cls, name):
+        """Checks enum for name"""
         return any(name == item.name for item in cls)
 
 _LEVEL_TO_FMT = {
-        VColors.CRITICAL.name: ('{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(message)s'.format(
-            VColors.CRITICAL.value, VColors.NORMAL.value, VColors.WARNING.value, VColors.GREEN.value)),
-        VColors.ERROR.name: ('{0}ERROR{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
-            VColors.ERROR.value, VColors.NORMAL.value, VColors.WARNING.value, VColors.GREEN.value)),
-        VColors.WARNING.name: '{0}WARNING{1}:%(msg)s'.format(VColors.WARNING.value, VColors.NORMAL.value),
-        VColors.INFO.name: '{0}%(message)s{1}'.format(VColors.INFO.value, VColors.NORMAL.value),
-        VColors.DEBUG.name: '{0}DEBUG{1}:%(msg)s'.format(VColors.DEBUG.value, VColors.NORMAL.value)
-        }
+    VColors.CRITICAL.name:
+    ('{0}CRITICAL{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(message)s'.format(
+        VColors.CRITICAL.value, VColors.NORMAL.value,
+        VColors.WARNING.value, VColors.GREEN.value)),
+    VColors.ERROR.name:
+    ('{0}ERROR{1}:{2}%(module)s{1}:{3}%(lineno)d{1}:\t%(msg)s'.format(
+        VColors.ERROR.value, VColors.NORMAL.value, VColors.WARNING.value,
+        VColors.GREEN.value)),
+    VColors.WARNING.name:
+    '{0}WARNING{1}:%(msg)s'.format(VColors.WARNING.value,
+                                   VColors.NORMAL.value),
+    VColors.INFO.name: '{0}%(message)s{1}'.format(VColors.INFO.value,
+                                                  VColors.NORMAL.value),
+    VColors.DEBUG.name: '{0}DEBUG{1}:%(msg)s'.format(VColors.DEBUG.value,
+                                                     VColors.NORMAL.value)
+}
 
 # https://stackoverflow.com/a/384125
 class VFormatter(logging.Formatter):
+    """Logging Formatter Class"""
     def __init__(self, msg, use_color=True):
         logging.Formatter.__init__(self, msg)
         self.use_color = use_color
@@ -57,9 +68,9 @@ class VFormatter(logging.Formatter):
                 lineno = record.lineno
 #                record.lineno = VColors.GREEN.value + str(lineno)
                 record.module = (
-                        VColors.WARNING.value
-                        + module
-                        + VColors.GREEN.value)
+                    VColors.WARNING.value
+                    + module
+                    + VColors.GREEN.value)
             else:
                 record.lineno = 0
                 record.module = ""
