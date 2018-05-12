ff22a7a7cdd41d59cd9499ad881faad715aef92e
FrancescoMagliocco <Magliocco.W.Francesco@gmail.com>
2018-05-11 22:08:25 -0400

    Added documentation

--- scripts/libmp3lame.py
+++ scripts/libmp3lame.py
@@ -1,18 +1,28 @@
-#!/usr/bin/env python3
+# vim: se fenc=utf8 :
 """library mp3 lame"""
 
-import os
+__author__ = "Francesco Magliocco (aka Cmptr)"
+__license__ = "GPLv3"
+__version__ = "0.0.1"
+__maintainer__ = "Francesco Magliocco (aka Cmptr)"
+__status__ = "Development"
 
-CONFIG: str = ("mkdir -p build/libmp3lame/; cd build/libmp3lame; ../../repos/libmp3lame/configure "
-        + "--prefix='{0}' ".format(os.path.expandvars('${FF_PREFIX}'))
-        + "--build='{0}' ".format(os.path.expandvars('${FF_BUILD}'))
-        + "--host='{0}' ".format(os.path.expandvars('${FF_HOST}'))
-        + "--enable-static='no' "
-        + "--enable-nasm "
-        + "--disable-rpath "
-        + "--disable-cpml "
-        + "--disable-gtktest "
-        + "--disable-frontend "
-        + "--with-pic='pic' "
-        + "--with-sysroot='{0}' ".format(os.path.expandvars('${FF_SYS_ROOT}'))
-        + "CFLAGS='-m64'")
+import os.path
+import sys
+from repobase import RepoTool, RepoBase
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+class LibMP3Lame(RepoBase):
+    '''Test'''
+    def __init__(self):
+        super().__init__(
+            'libmp3lame',
+            RepoTool.SVN_TOOL,
+            'https://svn.code.sf.net/p/lame/svn/trunk/lame',
+            '--enable-libmp3lame')
+
+    def get_config(self):
+        raise NotImplementedError()
+
+    def get_repo_download(self):
+        raise NotImplementedError()
