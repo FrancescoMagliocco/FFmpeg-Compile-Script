d7cda468d3d80adbff8c08630fe3281d493f867b
FrancescoMagliocco <Magliocco.W.Francesco@gmail.com>
2018-05-17 14:28:36 -0400

    Added  '_is_dir()' and '_to_abspath()'.

--- scripts/repobase.py
+++ scripts/repobase.py
@@ -2,8 +2,10 @@
 """Repository Base"""
 
 import logging
+import os
 from enum import Enum
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 class RepoTool(Enum):
     """Repository Tools"""
@@ -37,8 +39,32 @@ class RepoBase(ABC):
         self.repo_url = repo_url
         self.switch = switch
 
+    @staticmethod
+    def _to_abspath(path_str):
+        logging.debug("Checking if '%s' is an absolute path...", path_str)
+        if os.path.isabs(path_str):
+            logging.debug("'%s' is an absolute path!", path_str)
+        else:
+            logging.warning("'%s' is not an absolute path!", path_str)
+            logging.debug("Getting absolute path for '%s'...", path_str)
+            path_str = os.path.abspath(path_str)
+            logging.debug('absolute path: %s', path_str)
+
+        return path_str
+
+    def _is_dir(self, path_str):
+        path_str = self._to_abspath(path_str)
+        logging.debug("Checking if '%s' is a directory...", path_str)
+        tmp_path = Path(path_str)
+        if tmp_path.is_dir():
+            logging.debug("'%s' is a directory!", tmp_path)
+            return True
+
+        logging.warning("'%s' is not a directory!", tmp_path)
+        return False
+
     @abstractmethod
-    def get_config(self):
+    def get_config(self, **kwargs):
         '''get configuration arguments'''
         raise NotImplementedError("'get_config()' Not Implemented!")
 
